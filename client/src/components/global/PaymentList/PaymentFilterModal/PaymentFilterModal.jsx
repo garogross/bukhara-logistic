5a12777084269155294751c6665d10c4b685cd6d
garogross <garikgevorgyan.rap@mail.ru>
2024-02-14 15:40:05 +0400

    fix filter modal bug

--- client/src/components/global/PaymentList/PaymentFilterModal/PaymentFilterModal.jsx
+++ client/src/components/global/PaymentList/PaymentFilterModal/PaymentFilterModal.jsx
@@ -78,7 +78,7 @@ function PaymentFilterModal({show, onClose, onSaveFilters,id}) {
             <Backdrop inProp={show} onClose={onClose} highZIndex={true}/>
             <NewPortalProvider>
                 <TransitionProvider
-                    className={`${styles["filterModal"]} popupBox`}
+                    className={`${styles["filterModal"]}`}
                     inProp={show}
                     style={'right'}
                 >
