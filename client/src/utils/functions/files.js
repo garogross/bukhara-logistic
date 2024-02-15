c27ec76e9302ca8d62cb141aea8a5d6f02700922
garogross <garikgevorgyan.rap@mail.ru>
2024-02-15 14:29:30 +0400

    update upload file logic

--- client/src/utils/functions/files.js
+++ client/src/utils/functions/files.js
@@ -9,7 +9,7 @@ export const getFileName = (file) => file.replace("/files/", "")
 export const downloadFilePath = (file) => `${proxy}${baseUrl}${downloadFileUrl}${getFileName(file)}`
 
 
-export const onUploadFile = (event,clb) => {
+export const onUploadFile = (event, clb) => {
     const selectedFiles = event.target.files;
     // Iterate through each selected file
     Array.from(selectedFiles).forEach((file) => {
@@ -19,50 +19,21 @@ export const onUploadFile = (event,clb) => {
             reader.onload = (e) => {
                 const img = new Image();
                 img.onload = () => {
-                    // Use exif-js to read EXIF data
                     EXIF.getData(file, function () {
                         const orientation = EXIF.getTag(this, 'Orientation');
                         const canvas = document.createElement('canvas');
                         const ctx = canvas.getContext('2d');
-
                         if ([5, 6, 7, 8].indexOf(orientation) > -1) {
-                            canvas.width = img.height;
-                            canvas.height = img.width;
+                            canvas.width = img.width;
+                            canvas.height = img.height;
                         } else {
                             canvas.width = img.width;
                             canvas.height = img.height;
                         }
 
-                        // Apply rotation based on EXIF orientation
-                        switch (orientation) {
-                            case 2:
-                                ctx.transform(-1, 0, 0, 1, img.width, 0);
-                                break;
-                            case 3:
-                                ctx.transform(-1, 0, 0, -1, img.width, img.height);
-                                break;
-                            case 4:
-                                ctx.transform(1, 0, 0, -1, 0, img.height);
-                                break;
-                            case 5:
-                                ctx.transform(0, 1, 1, 0, 0, 0);
-                                break;
-                            case 6:
-                                ctx.transform(0, 1, -1, 0, img.height, 0);
-                                break;
-                            case 7:
-                                ctx.transform(0, -1, -1, 0, img.height, img.width);
-                                break;
-                            case 8:
-                                ctx.transform(0, -1, 1, 0, 0, img.width);
-                                break;
-                            default:
-                                break;
-                        }
 
-                        // Draw the image on the canvas
                         ctx.drawImage(img, 0, 0);
-                        // Convert canvas back to a Blob
+
                         canvas.toBlob((blob) => {
                             const rotatedFile = new File([blob], file.name, {
                                 type: 'image/jpeg',
